1e8dded8305c81e22dc8ed30e6a312a684b2fd6d
eliluminado <eliluminado@codigopython.com.ar>
2015-12-22 19:34:46 -0300

    Ignoring warning on Sphinx

--- source/conf.py
+++ source/conf.py
@@ -5,6 +5,15 @@
 
 import os
 
+import sphinx.environment
+from docutils.utils import get_source_line
+
+def _warn_node(self, msg, node):
+    if not msg.startswith('nonlocal image URI found:'):
+        self._warnfunc(msg, '%s:%s' % get_source_line(node))
+
+sphinx.environment.BuildEnvironment.warn_node = _warn_node
+
 extensions = [
     # 'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
